a28031e131f0fcad93a8c24d2d00603bf66b840c
lizhaochao <lzc@lizhaochao.com>
2021-05-28 10:30:12 +0800

    add more hash algs

--- lib/exhmac/const.ex
+++ lib/exhmac/const.ex
@@ -10,5 +10,13 @@ defmodule ExHmac.Const do
   def default_timestamp_offset_seconds, do: 900
   def default_nonce_ttl, do: 900
 
-  def support_hash_algs, do: [:sha512, :sha256, :md5]
+  def support_hash_algs do
+    with sha1 <- [:sha],
+         sha2 <- [:sha512, :sha384, :sha256, :sha224],
+         sha3 <- [:sha3_512, :sha3_384, :sha3_256, :sha3_224],
+         blake2 <- [:blake2b, :blake2s],
+         compatibility_only_hash <- [:md5] do
+      sha1 ++ sha2 ++ sha3 ++ blake2 ++ compatibility_only_hash
+    end
+  end
 end
