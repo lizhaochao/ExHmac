171f07e8085ad33b8f4feea7c6a213f4f0c9c67d
lizhaochao <lzc@lizhaochao.com>
2021-05-31 00:57:06 +0800

    improve ExHmac.Repo

--- lib/exhmac/repo.ex
+++ lib/exhmac/repo.ex
@@ -2,21 +2,6 @@ defmodule ExHmac.Repo do
   @moduledoc false
   alias ExHmac.KVRepo
 
-  ###
-  def create(key, value, _config) do
-    fun = fn current ->
-      new = if is_nil(current), do: value, else: current
-      {current, new}
-    end
-
-    key
-    |> KVRepo.get_and_update(fun)
-    |> case do
-      {new, _} when new == value -> :ok
-      _ -> :error
-    end
-  end
-
   def get(key, _config) do
     KVRepo.fetch(key)
   end
@@ -27,10 +12,5 @@ defmodule ExHmac.Repo do
 
   def delete(keys, _config) do
     KVRepo.drop(keys)
-  end
-
-  ###
-  def get_and_update(key, fun, _config) do
-    KVRepo.get_and_update(key, fun)
   end
 end
